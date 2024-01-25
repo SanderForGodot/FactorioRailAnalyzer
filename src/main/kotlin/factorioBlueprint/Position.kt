5b3723656e607d451fe1e1ba07543acfba32bfd8
Sander <sander.stella00@gmail.com>
2024-01-25 11:40:03 +0100

    added operator fun minusAssign(other: Position) to Position.kt

--- src/main/kotlin/factorioBlueprint/Position.kt
+++ src/main/kotlin/factorioBlueprint/Position.kt
@@ -5,29 +5,35 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 
-data class Position (
+data class Position(
 
-  @SerializedName("x" ) var x : Double ,
-  @SerializedName("y" ) var y : Double
+    @SerializedName("x") var x: Double,
+    @SerializedName("y") var y: Double
 
 ) {
-  operator fun plus(position: Position): Position {
-   return Position(
-      x+ position.x,
-     y+ position.y
-    )
-  }
-    fun distanceTo(position:Position ): Double {
+    operator fun plus(position: Position): Position {
+        return Position(
+            x + position.x,
+            y + position.y
+        )
+    }
+
+    fun distanceTo(position: Position): Double {
         val yDifference = (position.y - this.y).pow(2)
         val xDifference = (position.x - this.x).pow(2)
         return sqrt((yDifference + xDifference))
     }
 
     override fun equals(other: Any?): Boolean {
-        if(other !is Position)
+        if (other !is Position)
             return false;
         val other: Position = other as Position
 
         return x == other.x && y == other.y;
+    }
+
+    operator fun minusAssign(other: Position) {
+        this.x -= other.x
+        this.y -= other.y
     }
 }
