8e38ddd137a4027d859ebfe154acefc3dac5e360
Sander <sander.stella00@gmail.com>
2024-01-25 10:03:40 +0100

    added doesCollide(other: Edge) added doesCollide(other: Block)

--- src/main/kotlin/Block.kt
+++ src/main/kotlin/Block.kt
@@ -1,6 +1,22 @@
-import java.util.*
-
 class Block {
 
     lateinit var edgeList : ArrayList<Edge>
+
+
+
+    fun doesCollide(other: Edge): Boolean {
+       edgeList.forEach{
+           if (it.doesCollide(other))
+               return true
+       }
+        return false
+    }
+    fun doesCollide(other: Block): Boolean {
+        other.edgeList.forEach{
+            if (this.doesCollide(it))
+                return true
+        }
+        return false
+    }
+
 }
